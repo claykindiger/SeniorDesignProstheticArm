bc9129e4592c618be35841b637f443f760da6203
jetson <cak10@rice.edu>
2024-02-11 15:04:37 -0600

    add viz experiment re results

--- VisualInput/vision_experiment.py
+++ VisualInput/vision_experiment.py
@@ -1,15 +1,19 @@
 import math
+from datetime import datetime 
 from jetson_inference import detectNet
 from jetson_utils import videoSource, videoOutput, saveImage, Log
 
 Log.SetLevel('Error')
 
-net = detectNet("ssd-mobilenet-v2", threshold=0.5)
+net = detectNet("ssd-mobilenet-v2", threshold=0.7)
 
-num_trials = 10
+num_trials = 5
 
-def classify_image(net):
-    camera = videoSource("/dev/video1")
+def classify_image(net,trial_num):
+    camera = videoSource("/dev/video0")
+    
+    center_frame = (camera.GetWidth()/2,camera.GetHeight()/2)
+    print(center_frame)
 
     detections = []
 
@@ -21,15 +25,16 @@ def classify_image(net):
 		
         detections = net.Detect(img)
     
-    saveImage("test_pic.jpg",img)
+    saveImage(f"experiment/test_pic_{trial_num}.jpg",img)
 
     print("Saved resulting picture in test_pic.jpg")
 	
     min_label = None
-    min_distance = 1000
+    min_distance = 1000000
     for det in detections:
-        center = det.center
-        distance = math.sqrt(center[0]**2 + center[1]**2)
+        center = det.Center
+        distance = math.sqrt((center[0]- center_frame[0])**2 + (center[1] - center_frame[1])**2)
+        print(net.GetClassLabel(det.ClassID), distance,center)
         if distance < min_distance:
             min_distance = distance
             min_label = net.GetClassLabel(det.ClassID)
@@ -40,15 +45,22 @@ def classify_image(net):
 
 results = []
 
-def run_trial(results,correct_label):
-    starttime = time.time()
-    label = classify_image(None)
-    runtime = time.time() - starttime
+def run_trial(net, trial_num, results,correct_label):
+    start = datetime.now()
+    label = classify_image(net,trial_num)
+    end = datetime.now()
+    runtime = end - start
     
     # results.append([label, correct_label, runtime])
     return [label, correct_label, runtime]
-print('label     correct label      runtime')
+
+print('trial num    label     correct label      runtime')
+
 for i in range(num_trials):
-    results = run_trial(results,'bottle')
-    print(f'{results[0]}     {results[1]}      {results[2]}')
+    results = run_trial(net, i, results,'bottle')
+    lab = results[0]
+    corr_lab = results[1]
+    time = results[2]
+    print(i,lab,corr_lab,time)
+    print()
     
